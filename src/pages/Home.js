9aae9d9c856b7aa6f913d4c4672aa558e6a74ab6
unknown <grimaldomarkdaryl@gmail.com>
2023-10-17 23:41:16 +0800

    Added style on text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,24 @@ const Home = () => {
   return (
     <div className={classes.home__wrapper}>
       <NavBar />
-      <h5> So, you want to travel to</h5>
-      <h1> Space</h1>
-      <p>
-        Let’s face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </p>
+      <div className={classes.home__content}>
+        <h5
+          className={`${classes.home__heading__five} ${classes.page__content}`}
+        >
+          So, you want to travel to
+        </h5>
+        <h1
+          className={`${classes.home__heading__one} ${classes.page__content}`}
+        >
+          Earth
+        </h1>
+        <p className={`${classes.home__p} `}>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
     </div>
   );
 };
